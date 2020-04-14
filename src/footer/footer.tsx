40e8d669cab8967b44b853624b7c87b3c60a65ed
ribalkoko48 <kotara22@rambler.ru>
2020-04-14 10:23:46 +0300

    revert to jsx

--- src/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import style from './footer.module.scss';
-
-export function Footer() {
-    return (
-        <div className={style.footer}>
-        </div>
-    )
-};
